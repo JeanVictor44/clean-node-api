5133b4273e6853da13cd268f35349024c4c0da8e
JeanVictor44 <jeanvictorgithubprojects@gmail.com>
2024-01-14 20:12:26 -0300

    test: ensure SignUpController returns if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -21,5 +21,25 @@ describe('SignUp Controller', () => {
     // handle é a função do controlador que recebe o request e response e processa para retornar algo
     const httResponse = sut.handle(httpRequest)
     expect(httResponse.statusCode).toBe(400)
+    // não utilizar toBe para comparar objetos porque ele compara por ponteiro também
+    expect(httResponse.body).toEqual(new Error('Missing param: name'))
+  })
+
+  test('should return 400 if no email is provided', () => {
+    /* como a implementação está amarrada a um retorno somos obrigados a implementar
+     um código que funcione no caso em que qualquer parâmetro obrigatório não seja fornecido
+    */
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httResponse = sut.handle(httpRequest)
+    expect(httResponse.statusCode).toBe(400)
+    expect(httResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
