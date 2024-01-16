373a5378a20aac8d72263116c425478ee8ce061c
JeanVictor44 <jeanvictorgithubprojects@gmail.com>
2024-01-15 21:49:04 -0300

    test: ensure SignUpController return 400 if an invalid email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,4 +1,6 @@
+import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
+import { EmailValidator } from '../protocols/email-validator'
 import { SignUpController } from './signup'
 
 /* Rota de cadastro
@@ -7,13 +9,35 @@ import { SignUpController } from './signup'
     Validar que os parâmetro irão vir corretamento do client(usuário)
     SUT -> system under test -> classe que está sendo testada no momento
 */
-const makeSut = (): SignUpController => {
-  return new SignUpController()
+// Esse factory diminui a repetição em precisar injetar as dependências em cada um dos testes
+interface SutTypes {
+  sut: SignUpController
+  emailValidatorStub: EmailValidator
+}
+const makeSut = (): SutTypes => {
+  // STUB -> Funções com retornos predefinidos
+  // Tanto faz a implementação do EmailValidator, só precisamos do retorno true ou false
+  // para saber como o SignUpController irá lidar com esses dois retornos
+  class EmailValidatorStub implements EmailValidator {
+    // O ideal é que esse STUB sempre retorne true para os outros testes não quebarem
+    // Apenas em testes específicos invertemos o valor desse mock
+    // Sempre inicializar mock com o valor verdadeiro
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  const emailValidatorStub = new EmailValidatorStub()
+  const sut = new SignUpController(emailValidatorStub)
+
+  return {
+    sut,
+    emailValidatorStub
+  }
 }
 
 describe('SignUp Controller', () => {
   test('should return 400 if no name is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
@@ -28,9 +52,8 @@ describe('SignUp Controller', () => {
     // não utilizar toBe para comparar objetos porque ele compara por ponteiro também
     expect(httResponse.body).toEqual(new MissingParamError('name'))
   })
-
   test('should return 400 if no email is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -44,7 +67,7 @@ describe('SignUp Controller', () => {
     expect(httResponse.body).toEqual(new MissingParamError('email'))
   })
   test('should return 400 if no password is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -58,7 +81,7 @@ describe('SignUp Controller', () => {
     expect(httResponse.body).toEqual(new MissingParamError('password'))
   })
   test('should return 400 if no passwordConfirmation is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -69,5 +92,21 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+  })
+  test('should return 400 if an invalid email is provided', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    // Espiona um método e altera o valor padrão retornado por ele
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'invalid_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
 })
